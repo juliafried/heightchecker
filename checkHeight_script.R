5fd69b02c5e74cee3466b7d4c68c46fa87fff914
Julia <julia.fried2@gmail.com>
2017-11-02 11:23:31 +0100

    changed comments and apply instead of for

--- checkHeight_script.R
+++ checkHeight_script.R
@@ -5,46 +5,38 @@
 
 library(dplyr)
 
-
+# Einlesen der Daten
 age = c(19, 22, 21, 23, 22, 20, 28, 25)
 weight = c(50, 75, 80, 56, 75, 58, 65, 82)
 height = c(1.66, 1.78, 1.90, 1.72, 1.83, 1.68, 1.70, 1.85)
 sex = c("F", "M", "M", "F", "M", "F", "F", "M")
 
+# Kombinieren der Daten in einen data frame
 students = data.frame(cbind(age, weight, height, sex))
+
+# Transformation der Cols in das richtige Format
 students = transform(students, age = as.numeric(as.character(age)))
 students = transform(students, height = as.numeric(as.character(height)))
 students = transform(students, weight = as.numeric(as.character(weight)))
 
+# Benennung der einzelnen Namen
 students$name = c("Maria", "Franz", "Peter", "Lisa", "Hans", "Eva", "Mia", "Karl")
 
-
-
+# Funktion zur Berechnung der Groessenunterschiede (pro F / M)
 checkHeight3 = function(students.input = students){
-
-  result.frame = data.frame(matrix(NA, nrow = nrow(students.input), ncol = 2))
-  colnames(result.frame) = c("name", "difference")
-
-  male.mean = students.input %>%
-    filter(sex == "M") %>%
-    summarise(mean = mean(height))
-  female.mean = students.input %>%
-    filter(sex == "F") %>%
-    summarise(mean = mean(height))
-
-  for (i in 1:nrow(students.input)) {
-    # calculate sex-specific deviations from the mean
-    if (students.input[i, "sex"] == "F") {
-      height.diff = 100*(students.input[i,]$height - female.mean$mean)
+  heightVec = apply(students, MARGIN = 1, FUN = function(student){
+    if (student["sex"] == "M") {
+      mean(students$height[students$sex == "M"]) - as.numeric(student["height"])
+    } else {
+      mean(students$height[students$sex == "F"]) - as.numeric(student["height"])
     }
-    else {
-      height.diff = 100*(students.input[i, ]$height - male.mean$mean)
-    }
-    result.frame[i, "name"] = as.character(students.input[i, "name"])
-    result.frame[i, "difference"] = height.diff
-  }
-  return(result.frame)
+  })
+  out = data.frame("name" = students$name, "diff" = heightVec)
+  return(out)
 }
 
+# Ausfuehren der Fkt.
 checkHeight3(students.input = students)
 
+
+   
